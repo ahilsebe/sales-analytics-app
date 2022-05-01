f1dcc47ea8a188fb38d0466d53be2c5c3b375880
Andrew Hilseberg <andrewhilseberg@andrews-mbp-2.lan>
2022-05-01 15:47:07 -0400

    5/1/22 v2

--- src/App.js
+++ src/App.js
@@ -29,14 +29,29 @@ function App() {
   return (
 
 
-  
-
-
    <div className="dashboard"> 
+
+<input type="checkbox" id="active"/>
+          <label for="active" class="menu-btn"><span></span></label>
+          <label for="active" class="close"></label>
+          <div class="wrapper">
+            <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Gallery</a></li>
+      <li><a href="#">Feedback</a></li>
+      </ul>
+      </div>
+    
+
+
+
       <div className="title">
           <h1>Sales Analytics</h1>
           <p>An analysis of monthly sales data</p>
       </div>
+      
       <div class="chart-container-alt">
         <div class="chart-title">
           <h2>Commentary</h2>
